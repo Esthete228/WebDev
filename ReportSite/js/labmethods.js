696d94bf688aa2189535a6b0b4fcb70248e75bd2
Esthete228 <anatoliidudik@gmail.com>
2020-10-24 17:49:58 +0300

    Report Site

---
+++ ReportSite/js/labmethods.js
@@ -0,0 +1,235 @@
+//Lab1
+    function description(){
+        var text = document.getElementById("t1");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        var pic2 = document.createElement("IMG");
+        var pic3 = document.createElement("IMG");
+        var pic4 = document.createElement("IMG");
+        var pic5 = document.createElement("IMG");
+        var pic6 = document.createElement("IMG");
+        var pic7 = document.createElement("IMG");
+        var pic8 = document.createElement("IMG");
+        var pic9 = document.createElement("IMG");
+        var pic10 = document.createElement("IMG");
+        var pic11 = document.createElement("IMG");
+        pic1.src = "images/Lab1/description_1.jpg";
+        pic2.src = "images/Lab1/description_2.jpg";
+        pic3.src = "images/Lab1/table_1.jpg";
+        pic4.src = "images/Lab1/table_2.jpg";
+        pic5.src = "images/Lab1/table_3.jpg";
+        pic6.src = "images/Lab1/table_4.jpg";
+        pic7.src = "images/Lab1/table_5.jpg";
+        pic8.src = "images/Lab1/table_6.jpg";
+        pic9.src = "images/Lab1/table_7.jpg";
+        pic10.src = "images/Lab1/description_3.jpg";
+        pic11.src = "images/Lab1/diagram.jpg";
+        text.appendChild(pic1);
+        text.appendChild(pic2);
+        text.appendChild(pic3);
+        text.appendChild(pic4);
+        text.appendChild(pic5);
+        text.appendChild(pic6);
+        text.appendChild(pic7);
+        text.appendChild(pic8);
+        text.appendChild(pic9);
+        text.appendChild(pic10);
+        text.appendChild(pic11);
+    }
+     function mission(){
+    	document.getElementById("t1").innerHTML = "Постановкою нашої роботи є створення інтернет магазину у якому забезпечений функціонал купівлі та продажу різних товарів";
+    }
+    function mission_lab1(){
+        document.getElementById("t1").innerHTML = "Постановка задачі: Придбати практичні навички роботи  з html-документом, таблицями,формами, зображеннями, посиланнями";
+    }
+    function structure(){
+    	var text = document.getElementById("t1");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        var pic2 = document.createElement("IMG");
+        var pic3 = document.createElement("IMG");
+        var pic4 = document.createElement("IMG");
+        var pic5 = document.createElement("IMG");
+        var pic6 = document.createElement("IMG");
+        var pic7 = document.createElement("IMG");
+        var pic8 = document.createElement("IMG");
+        pic1.src = "images/Lab1/structure_1.jpg";
+        pic2.src = "images/Lab1/structure_2.jpg";
+        pic3.src = "images/Lab1/structure_3.jpg";
+        pic4.src = "images/Lab1/structure_4.jpg";
+        pic5.src = "images/Lab1/structure_5.jpg";
+        pic6.src = "images/Lab1/structure_6.jpg";
+        pic7.src = "images/Lab1/structure_7.jpg";
+        pic8.src = "images/Lab1/structure_8.jpg";
+        text.appendChild(pic1);
+        text.appendChild(pic2);
+        text.appendChild(pic3);
+        text.appendChild(pic4);
+        text.appendChild(pic5);
+        text.appendChild(pic6);
+        text.appendChild(pic7);
+        text.appendChild(pic8);
+    }
+    function table(){
+    	var text = document.getElementById("t1");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        pic1.src = "images/Lab1/table.jpg";
+        text.appendChild(pic1);
+    }
+    function formCode(){
+    	var text = document.getElementById("t1");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        pic1.src = "images/Lab1/form.jpg";
+        text.appendChild(pic1);
+    }
+    function picture(){
+    	var text = document.getElementById("t1");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        pic1.src = "images/Lab1/home.jpg";
+        text.appendChild(pic1);
+    }
+//Lab2
+    function mission_lab2(){
+        document.getElementById("t2").innerHTML = "Постановка задачі: придбати практичні навички роботи  з селекторами,ідентифікаторами, списками,  різноманітними властивостями кольору і фону,  зовнішними та внутрішними  відступами,  плаваючими елементами, оформленням текстових елементів";
+    }
+    function lists(){
+        var text = document.getElementById("t2");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        var pic2 = document.createElement("IMG");
+        pic1.src = "images/Lab2/Lists/list_1.jpg"
+        pic2.src = "images/Lab2/Lists/list_2.jpg"
+        text.appendChild(pic1);
+        text.appendChild(pic2);
+    }
+    function identificators(){
+        var text = document.getElementById("t2");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        pic1.src = "images/Lab2/Identificator.jpg"
+        text.appendChild(pic1);
+    }
+    function paddings(){
+        var text = document.getElementById("t2");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        var pic2 = document.createElement("IMG");
+        pic1.src = "images/Lab2/Paddings/1.jpg"
+        pic2.src = "images/Lab2/Paddings/2.jpg"
+        text.appendChild(pic1);
+        text.appendChild(pic2);
+    }
+    function floatElements(){
+        var text = document.getElementById("t2");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        var pic2 = document.createElement("IMG");
+        var pic3 = document.createElement("IMG");
+        pic1.src = "images/Lab2/floatElements/1.jpg"
+        pic2.src = "images/Lab2/floatElements/2.jpg"
+        pic3.src = "images/Lab2/floatElements/3.jpg"
+        text.appendChild(pic1);
+        text.appendChild(pic2);
+        text.appendChild(pic3);
+    }
+    function tagClassSelectors(){
+        var text = document.getElementById("t2");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        var pic2 = document.createElement("IMG");
+        var pic3 = document.createElement("IMG");
+        var pic4 = document.createElement("IMG");
+        pic1.src = "images/Lab2/TagAndClass/clas_1.jpg"
+        pic2.src = "images/Lab2/TagAndClass/clas_2.jpg"
+        pic3.src = "images/Lab2/TagAndClass/clas_3.jpg"
+        pic4.src = "images/Lab2/TagAndClass/tag.jpg"
+        text.appendChild(pic1);
+        text.appendChild(pic2);
+        text.appendChild(pic3);
+        text.appendChild(pic4);
+    }
+    function otherSelectors(){
+        var text = document.getElementById("t2");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        pic1.src = "images/Lab2/otherSelectors.jpg"
+        text.appendChild(pic1);
+
+    }
+    function selectorsGrouping(){
+        var text = document.getElementById("t2");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        var pic2 = document.createElement("IMG");
+        var pic3 = document.createElement("IMG");
+        var pic4 = document.createElement("IMG");
+        var pic5 = document.createElement("IMG");
+        pic1.src = "images/Lab2/selectorsGrouping/1.jpg"
+        pic2.src = "images/Lab2/selectorsGrouping/2.jpg"
+        pic3.src = "images/Lab2/selectorsGrouping/3.jpg"
+        pic4.src = "images/Lab2/selectorsGrouping/4.jpg"
+        pic5.src = "images/Lab2/selectorsGrouping/5.jpg"
+        text.appendChild(pic1);
+        text.appendChild(pic2);
+        text.appendChild(pic3);
+        text.appendChild(pic4);
+        text.appendChild(pic5);
+    }
+//Lab3
+    function mission_lab3(){
+        document.getElementById("t3").innerHTML = "Постановка задачі: придбати практичні навички роботи  верстки сторінок засобами CSS, верстки на основі плаваючих елементів, з’ясувати переваги та недоліки типів макетів веб-сторінок.";
+    }
+    function frontMaket(){
+        var text = document.getElementById("t3");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        var pic2 = document.createElement("IMG");
+        pic1.src = "images/Lab3/Meta1.jpg"
+        pic2.src = "images/Lab3/Model.jpg"
+        text.appendChild(pic1);
+        text.appendChild(pic2);
+    }
+    function tableMarkup(){
+        var text = document.getElementById("t3");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        pic1.src = "images/Lab3/Tables.jpg"
+        text.appendChild(pic1);
+    }
+    function floatMarkup(){
+        var text = document.getElementById("t3");
+        text.innerHTML = "";
+        var pic1 = document.createElement("IMG");
+        pic1.src = "images/Lab3/Blocks.jpg"
+        text.appendChild(pic1);
+    }
+//Lab4
+    function mission_lab4(){
+        document.getElementById("t4").innerHTML = "придбати практичні навички роботи маніпулювання інформаційним вмістом Web-документа засобами мови JavasSript, використання об’єктів,масивів, функцій, подій, обробників подій у сценаріях на мові JavasSript";
+
+    }
+    function scheme(){
+
+    }
+    function hanlerExample(){
+
+    }
+    function entity(){
+
+    }
+    function insertion(){
+
+    }
+    function task_4(){
+
+    }
+    function task_6(){
+
+    }
+    function conclution(){
+
+    }
+//Lab5
